660f59c36ae305d8528df8fabe0aeda883e3a69c
Victor <victormovileanu@gmail.com>
2025-08-24 14:43:38 +0200

    configure dexie-cloud

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,16 +1,22 @@
 import Dexie, { type EntityTable } from 'dexie';
 import type { BookmarkData } from '$lib/types/bookmark.js';
 import './dexie-uuid-plugin.js';
+import dexieCloud from 'dexie-cloud-addon';
 
 class Database extends Dexie {
 	bookmarks!: EntityTable<BookmarkData, 'id'>;
 
 	constructor() {
-		super('BookmarkManager');
+		super('BookmarkManager', { addons: [dexieCloud] });
 		this.version(1).stores({
-			bookmarks: '$$id, title, url, *tags, *keywords, isStarred, isReviewed, created, modified'
+			bookmarks: '@id, title, url, *tags, *keywords, isStarred, isReviewed, created, modified'
 		});
 	}
 }
 
 export const db = new Database();
+
+db.cloud.configure({
+	databaseUrl: 'https://z6n1slc6l.dexie.cloud',
+	requireAuth: true // optional
+});
