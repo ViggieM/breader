895d303086d24ac09c5a6e528699425798bde877
Victor <victormovileanu@gmail.com>
2025-12-10 14:25:37 +0100

    draft: This is how the tags could look like

---
+++ src/lib/components/TagMenu.svelte
@@ -0,0 +1,88 @@
+<script>
+</script>
+
+<ul class="menu rounded-box w-full p-0 mt-4">
+	<li>
+		<a>
+			<span class="icon-[ri--star-line] shrink-0"></span>
+			Favorites
+		</a>
+	</li>
+	<li>
+		<a>
+			<span class="icon-[ri--archive-2-line] shrink-0"></span>
+			Archive
+		</a>
+	</li>
+	<li>
+		<a>
+			<span class="icon-[ri--sticky-note-line] shrink-0"></span>
+			Notes
+		</a>
+	</li>
+	<li>
+		<a>
+			<span class="icon-[ri--price-tag-3-fill]"></span>
+			AI
+		</a>
+	</li>
+	<li>
+		<details>
+			<summary>
+				<span class="icon-[ri--price-tag-3-fill]"></span>
+				Frontend
+			</summary>
+			<ul>
+				<li>
+					<a>
+						<span class="icon-[ri--sticky-note-line] shrink-0"></span>
+						Thoughts on Frontend Development
+					</a>
+				</li>
+				<li>
+					<details>
+						<summary>
+							<span class="icon-[ri--price-tag-3-fill]"></span>
+							Svelte
+						</summary>
+						<ul>
+							<li>
+								<a>
+									<img
+										src="https://www.google.com/s2/favicons?domain=hynek.me"
+										alt=""
+										draggable="false"
+										loading="lazy"
+									/>
+									Production-ready Python Docker Containers with uv
+								</a>
+							</li>
+							<li>
+								<a>
+									<img
+										src="https://www.google.com/s2/favicons?domain=svelte.dev"
+										alt=""
+										draggable="false"
+										loading="lazy"
+									/>
+									Svelte Docs
+								</a>
+							</li>
+						</ul>
+					</details>
+				</li>
+			</ul>
+		</details>
+	</li>
+	<li>
+		<a>
+			<img
+				src="https://www.google.com/s2/favicons?domain=daisyui.com"
+				alt=""
+				draggable="false"
+				loading="lazy"
+			/>
+			DaisyUI
+		</a>
+	</li>
+</ul>
