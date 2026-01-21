28f4a44aa805f6d7491223cf5b02938ada3bfc33
Victor <victormovileanu@gmail.com>
2026-01-21 14:56:15 +0100

    reinit to indexdb version 1

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -3,7 +3,6 @@ import type { BookmarkData, TagData, FaviconData } from '$lib/types';
 import dexieCloud from 'dexie-cloud-addon';
 import { PUBLIC_DEXIE_CLOUD_DB_URL } from '$env/static/public';
 import type { NoteData } from '$lib/types/note';
-import { BookmarkStatus } from '$lib/types/bookmark';
 
 class Database extends Dexie {
 	bookmarks!: EntityTable<BookmarkData, 'id'>;
@@ -30,52 +29,7 @@ db.cloud.configure({
 		}).then((res) => res.json())
 });
 
-db.version(5).stores({
-	bookmarks: '@id, title, url, *tags, *keywords, isStarred, isReviewed, created, modified',
-	tags: '@id, parentId, name, order',
-	lists: '@id'
-});
-
-db.version(6)
-	.stores({
-		bookmarks: '@id, title, url, *tags, *keywords, isStarred, created, modified, status',
-		tags: '@id, parentId, name, order'
-	})
-	.upgrade((trans) => {
-		return trans
-			.table('bookmarks')
-			.toCollection()
-			.modify((bookmark) => {
-				if (bookmark.isArchived) {
-					bookmark.status = BookmarkStatus.ARCHIVED;
-				} else if (bookmark.isReviewed) {
-					bookmark.status = BookmarkStatus.READ;
-				} else if (bookmark.isReading) {
-					bookmark.status = BookmarkStatus.READING;
-				} else {
-					bookmark.status = BookmarkStatus.WANT_TO_READ;
-				}
-
-				delete bookmark.isArchived;
-				delete bookmark.isReviewed;
-				delete bookmark.isReading;
-			});
-	});
-
-db.version(7).stores({
-	bookmarks: '@id, title, url, *tags, *keywords, isStarred, created, modified, status',
-	tags: '@id, parentId, name, order',
-	lists: null, // Remove lists table
-	notes: '@id, *bookmarks, created, modified' // Add notes table
-});
-
-db.version(8).stores({
-	bookmarks: '@id, title, url, *tags, *keywords, isStarred, created, modified, status',
-	tags: '@id, parentId, name, order',
-	notes: '@id, *bookmarks, created, modified, title' // Add title field to notes
-});
-
-db.version(9).stores({
+db.version(1).stores({
 	bookmarks: '@id, title, url, *tags, *keywords, isStarred, created, modified, status',
 	tags: '@id, parentId, name, order',
 	notes: '@id, *bookmarks, created, modified, title',
